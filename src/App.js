c56afa709f359af1e8c8d82ca21f476ab53eaf75
rrm1 <rustamramazanovworkmail@gmail.com>
2021-08-09 17:10:05 +0300

    (add)base app struct

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import './App.css';
 
 const App = () => {
   return(
-    <div>
-      
+    <div className="app-wrapper">
+      <header className="header">
+        <img className="logo" src="https://s3-symbol-logo.tradingview.com/facebook--600.png" />
+      </header>
+
+      <sidebar className="sidebar">
+        <nav className="nav">
+
+        </nav>
+      </sidebar>
+      <section className="content-section">
+        <div className="content">
+          
+        </div>
+      </section>
     </div>
   );
 }
