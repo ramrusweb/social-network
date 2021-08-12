c1a6f21cfe2c753e5744dcdce61fbdd4645090bb
rrm1 <rustamramazanovworkmail@gmail.com>
2021-08-12 16:21:11 +0300

    (add)base structure spa

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ const App = () => {
   return(
     <div className="app-wrapper">
       <header className="header">
-        <img className="logo" src="https://s3-symbol-logo.tradingview.com/facebook--600.png" />
+        <div className="logo">
+          <img src="https://s3-symbol-logo.tradingview.com/facebook--600.png" />
+        </div>
       </header>
 
       <sidebar className="sidebar">
-        <nav className="nav">
-
+        <nav className="nav-menu">
+          <ul>
+            <li><a href="#s">Profile</a></li>
+            <li><a href="#s">Messages</a></li>
+            <li><a href="#s">News</a></li>
+            <li><a href="#s">Music</a></li>
+            <li><a href="#s">Settings</a></li>
+          </ul>
         </nav>
       </sidebar>
+
       <section className="content-section">
         <div className="content">
-          
+          <h1 className="main-title">Main Content</h1>
         </div>
       </section>
     </div>
