3427d8a3672046f336849c9ea0776ff024f9d9ba
ramrusgit <rustamramazanovworkmail@gmail.com>
2021-09-04 04:22:07 +0300

    (change)rewrote the styles for the module Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,15 +1,15 @@
-import './Profile.css';
+import ProfileStyle from './Profile.module.css';
 
 const Profile = () => {
   return(
-    <section className="content-section">
+    <section className={ProfileStyle.content__section}>
       <div className="content">
-        <img className="content-logo" src="https://www.strunkmedia.com/wp-content/uploads/2018/01/digital-ads-header.jpg" />
+        <img className={ProfileStyle.content__logo} src="https://www.strunkmedia.com/wp-content/uploads/2018/01/digital-ads-header.jpg" />
 
-        <div className="content-bio">
-          <img className="content-avatar" src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop" />
+        <div className="content__bio">
+          <img className={ProfileStyle.content__avatar} src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop" />
           
-          <div className="content-info">
+          <div className={ProfileStyle.content__info}>
             <h1>Rustam R.</h1><br></br>
             <span>Date of Birth: 12 September, 1999</span><br></br>
             <span>City: Kizlyar, Republic of Dagestan</span><br></br>
