d4e4f9505c56d573daad137047b7af3c9c898264
ramrusgit <rustamramazanovworkmail@gmail.com>
2021-09-04 04:00:22 +0300

    (change)move components by directories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import Profile from './components/Profile';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import Profile from './components/Profile/Profile';
 
 const App = () => {
   return(
