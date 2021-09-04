3cd5c90d0d343585f8cc07e6d9f2a5f713feb64e
ramrusgit <rustamramazanovworkmail@gmail.com>
2021-09-04 04:31:00 +0300

    (change)rewrote the styles for the module App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import AppStyle from './App.module.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Profile from './components/Profile/Profile';
 
 const App = () => {
   return(
-    <div className="app-wrapper">
+    <div className={AppStyle.app__wrapper}>
       <Header />
       <Sidebar />
       <Profile />
