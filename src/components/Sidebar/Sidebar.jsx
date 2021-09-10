19e4537f57126e429b755e57f35f2c81df88e838
ramrusgit <rustamramazanovworkmail@gmail.com>
2021-09-10 22:16:08 +0300

    (add)activeClassName for Sidebar NavLink, (css)anim when clicking on a link

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,11 +6,11 @@ const Sidebar = () => {
     <sidebar className={SidebarStyle.sidebar}>
       <nav className={SidebarStyle.nav__menu}>
         <ul>
-          <li><NavLink to="/profile">Profile</NavLink></li>
-          <li><NavLink to="/dialogs">Messages</NavLink></li>
-          <li><NavLink to="/news">News</NavLink></li>
-          <li><NavLink to="/music">Music</NavLink></li>
-          <li><NavLink to="/settings">Settings</NavLink></li>
+          <li><NavLink to="/profile" activeClassName={SidebarStyle.active_link}>Profile</NavLink></li>
+          <li><NavLink to="/dialogs" activeClassName={SidebarStyle.active_link}>Messages</NavLink></li>
+          <li><NavLink to="/news" activeClassName={SidebarStyle.active_link}>News</NavLink></li>
+          <li><NavLink to="/music" activeClassName={SidebarStyle.active_link}>Music</NavLink></li>
+          <li><NavLink to="/settings" activeClassName={SidebarStyle.active_link}>Settings</NavLink></li>
         </ul>
       </nav>
     </sidebar>
