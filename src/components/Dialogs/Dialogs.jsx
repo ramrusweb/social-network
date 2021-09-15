f2d9a8ef686f01024436bd88aebb9573d0db1c1a
ramrusgit <rustamramazanovworkmail@gmail.com>
2021-09-15 10:30:23 +0300

    (add)dialogs and messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,34 @@ import DialogsStyle from './Dialogs.module.css';
 const Dialogs = (props) => {
     return(
         <section className={DialogsStyle.dialogs}>
-            Dialogs
+            <div className={DialogsStyle.dialogs_items}>
+                <div className={DialogsStyle.dialog}>
+                    Ruslan
+                </div>
+                <div className={DialogsStyle.dialog + ' ' + DialogsStyle.active}>
+                    Kamila
+                </div>
+                <div className={DialogsStyle.dialog}>
+                    Evgeniy
+                </div>
+                <div className={DialogsStyle.dialog}>
+                    Alexander
+                </div>
+                <div className={DialogsStyle.dialog}>
+                    Said
+                </div>
+                <div className={DialogsStyle.dialog}>
+                    Alexandra
+                </div>
+                <div className={DialogsStyle.dialog}>
+                    Andrey
+                </div>
+            </div>
+            <div className={DialogsStyle.messages}>
+                <div className={DialogsStyle.message}>Hi</div>
+                <div className={DialogsStyle.message}>How are you?</div>
+                <div className={DialogsStyle.message}>Yo, man</div>
+            </div>
         </section>
     );
 }
