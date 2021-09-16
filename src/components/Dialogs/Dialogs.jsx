f327c213f118723a3a78b6af86a15f1117bf6290
ramrusgit <rustamramazanovworkmail@gmail.com>
2021-09-16 11:55:54 +0300

    (add)navlink for dialogs(plug)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,29 +1,30 @@
 import DialogsStyle from './Dialogs.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Dialogs = (props) => {
     return(
         <section className={DialogsStyle.dialogs}>
             <div className={DialogsStyle.dialogs_items}>
                 <div className={DialogsStyle.dialog}>
-                    Ruslan
+                    <NavLink to="/dialogs/1">Ruslan</NavLink>
                 </div>
-                <div className={DialogsStyle.dialog + ' ' + DialogsStyle.active}>
-                    Kamila
-                </div>
-                <div className={DialogsStyle.dialog}>
-                    Evgeniy
+                <div className={DialogsStyle.dialog + ' '+ DialogsStyle.active}>
+                    <NavLink to="/dialogs/2" className={DialogsStyle.active}>Kamila</NavLink>
                 </div>
                 <div className={DialogsStyle.dialog}>
-                    Alexander
+                    <NavLink to="/dialogs/3">Evgeniy</NavLink>
                 </div>
                 <div className={DialogsStyle.dialog}>
-                    Said
+                    <NavLink to="/dialogs/4">Alexander</NavLink>
                 </div>
                 <div className={DialogsStyle.dialog}>
-                    Alexandra
+                    <NavLink to="/dialogs/6">Said</NavLink>
                 </div>
                 <div className={DialogsStyle.dialog}>
-                    Andrey
+                    <NavLink to="/dialogs/7">Alexandra</NavLink>
+                </div>
+                <div className={DialogsStyle.dialog}>
+                    <NavLink to="/dialogs/8">Andrey</NavLink>
                 </div>
             </div>
             <div className={DialogsStyle.messages}>
