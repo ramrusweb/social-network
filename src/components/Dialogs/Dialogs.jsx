5c9b637973ae3ff6f1366de4f549a4b8ccdbd252
ramrusgit <rustamramazanovworkmail@gmail.com>
2021-09-26 19:50:03 +0300

    (ref)dialogs data

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,16 +3,27 @@ import Dialog from './Dialog/Dialog';
 import Message from './Message/Message';
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Ruslan'},
+        {id: 2, name: 'Kamila'},
+        {id: 3, name: 'Evgeniy'},
+        {id: 4, name: 'Alexander'},
+        {id: 5, name: 'Said'},
+        {id: 6, name: 'Alexandra'},
+        {id: 7, name: 'Andrey'},
+    ];
+
     return(
         <section className={DialogsStyle.dialogs}>
             <div className={DialogsStyle.dialogs_items}>
-                <Dialog id="1" name="Ruslan" />
-                <Dialog id="2" name="Kamila" />
-                <Dialog id="3" name="Evgeniy" />
-                <Dialog id="4" name="Alexander" />
-                <Dialog id="5" name="Said" />
-                <Dialog id="6" name="Alexandra" />
-                <Dialog id="7" name="Andrey" />
+                <Dialog id={dialogsData[0].id} name={dialogsData[0].name} />
+                <Dialog id={dialogsData[1].id} name={dialogsData[1].name} />
+                <Dialog id={dialogsData[2].id} name={dialogsData[2].name} />
+                <Dialog id={dialogsData[3].id} name={dialogsData[3].name} />
+                <Dialog id={dialogsData[4].id} name={dialogsData[4].name} />
+                <Dialog id={dialogsData[5].id} name={dialogsData[5].name} />
+                <Dialog id={dialogsData[6].id} name={dialogsData[6].name} />
             </div>
             <div className={DialogsStyle.messages}>
                 <Message message="Hi, how are you?" />
