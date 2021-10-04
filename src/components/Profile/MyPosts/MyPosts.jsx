9b838e1d51d95d9830aa1f218d68c9d96e0d3d44
ramrusgit <rustamramazanovworkmail@gmail.com>
2021-10-04 18:43:11 +0300

    (ref)MyPosts: variable names. postsElements map myPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,16 +4,18 @@ import AddPost from './Post/AddPost/AddPost';
 
 const MyPosts = () => {
 
-  let myPostsData = [
+  let myPosts = [
     {id: 1, message: "Hi, how are you?", likes: 15},
     {id: 2, message: "It's my first post", likes: 20}
   ];
 
+  let postsElements = myPosts
+    .map( p => <Post id={p.id} message={p.message} likes={p.likes} />);
+
   return(
     <section className={MyPostsStyle.myposts__wrapper}>
       <div className={MyPostsStyle.posts}>
-        <Post id={myPostsData[0].id} message={myPostsData[0].message} likes={myPostsData[0].likes} />
-        <Post id={myPostsData[1].id} message={myPostsData[1].message} likes={myPostsData[1].likes} />
+        {postsElements}
         <AddPost />
       </div>
     </section>
